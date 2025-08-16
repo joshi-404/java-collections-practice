3a731dd5a59a87fccc603c24079bca5650245895
Rudransh Joshi <rudranshjoshi108@gmail.com>
2025-08-17 02:25:17 +0530

    Update HashMapAndEqualsMethod.java

--- map/HashMapAndEqualsMethod.java
+++ map/HashMapAndEqualsMethod.java
@@ -1,1 +1,70 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
+public class HashMapAndEqualsMethod {
+    public static void main(String[] args) {
+        HashMap<Person,String> map=new HashMap<>();
+        Person p1=new Person("Alice",1);
+        Person p2=new Person("Bob",2);
+        Person p3=new Person("Alice",1);
+
+        map.put(p1,"Engineer");//hashcode1 --> index1
+        map.put(p2,"Designer");//hashcode2 --> index2
+        map.put(p3,"Manager");//hashcode3 --> index3  hashcode1 != hashcode3 because of object
+        //to make hashcode1 = hashcode3 write the hashcode and equals method in the Person class
+
+        System.out.println("HashMap size : "+map.size());
+        System.out.println("Value for p1 : "+map.get(p1));
+        System.out.println("Value for p3 : "+map.get(p3));
+        System.out.println(p1);
+
+        Map<String,Integer> map1=new HashMap<>();
+        map1.put("Shubham",90);//hashcode1 --> index1
+        map1.put("Neha",92);//hashcode2 --> index2
+        map1.put("Shubham",99);//hashcode1 --> index1 --> equals() -->replace
+
+    }
+}
+class Person{
+    private String name;
+    private int id;
+
+    public Person(String name,int id) {
+        this.name = name;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name,id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj) {
+            return true;
+        }
+        if(obj ==null){
+            return false;
+        }
+        if(getClass()!= obj.getClass()){
+            return false;
+        }
+        Person other=(Person)obj;
+        return id == other.getId() && Objects.equals(name,other.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "id : "+id+" , name : "+name ;
+    }
+}
